9332f92b712aad63420f339328c2877861cf35a9
jmbenso2 <gogglebrains@gmail.com>
2024-04-19 20:57:56 -0400

    Added Scope Lens and Choice Band to Victory Road; all type-boosting hold items for sale at Pokemon League

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1221,8 +1221,8 @@
 
 #define FLAG_ITEM_ROUTE_119_SOUL_DEW                                0x493
 #define FLAG_ITEM_NEW_MAUVILLE_LIGHT_BALL                           0x494
-#define FLAG_UNUSED_0x495                                           0x495 // Unused Flag
-#define FLAG_UNUSED_0x496                                           0x496 // Unused Flag
+#define FLAG_ITEM_VICTORY_ROAD_CHOICE_BAND                          0x495
+#define FLAG_ITEM_VICTORY_ROAD_SCOPE_LENS                           0x496
 #define FLAG_UNUSED_0x497                                           0x497 // Unused Flag
 #define FLAG_UNUSED_0x498                                           0x498 // Unused Flag
 #define FLAG_UNUSED_0x499                                           0x499 // Unused Flag
