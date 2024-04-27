05476f4794857c2c15f78e917de7c7a1c29fa272
jmbenso2 <gogglebrains@gmail.com>
2024-04-26 21:29:47 -0400

    Implemented Solid Rock and gave it to Camerupt as secondary ability.

--- src/data/text/abilities.h
+++ src/data/text/abilities.h
@@ -77,6 +77,7 @@ static const u8 sShellArmorDescription[] = _("Blocks critical hits.");
 static const u8 sCacophonyDescription[] = _("Avoids sound-based moves.");
 static const u8 sAirLockDescription[] = _("Negates weather effects.");
 static const u8 sSuperLuckDescription[] = _("Ups critical-hit rate.");
+static const u8 sSolidRockDescription[] = _("Weakens “supereffective”.");
 
 const u8 gAbilityNames[ABILITIES_COUNT][ABILITY_NAME_LENGTH + 1] =
 {
@@ -159,6 +160,7 @@ const u8 gAbilityNames[ABILITIES_COUNT][ABILITY_NAME_LENGTH + 1] =
     [ABILITY_CACOPHONY] = _("CACOPHONY"),
     [ABILITY_AIR_LOCK] = _("AIR LOCK"),
     [ABILITY_SUPER_LUCK] = _("SUPER LUCK"),
+	[ABILITY_SOLID_ROCK] = _("SOLID ROCK"),
 };
 
 const u8 *const gAbilityDescriptionPointers[ABILITIES_COUNT] =
@@ -242,4 +244,5 @@ const u8 *const gAbilityDescriptionPointers[ABILITIES_COUNT] =
     [ABILITY_CACOPHONY] = sCacophonyDescription,
     [ABILITY_AIR_LOCK] = sAirLockDescription,
     [ABILITY_SUPER_LUCK] = sSuperLuckDescription,
+	[ABILITY_SOLID_ROCK] = sSolidRockDescription,
 };
